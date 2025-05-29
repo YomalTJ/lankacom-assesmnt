380505c0cdd9f144d5b7f80dbd4de87bad928606
yomal-at-275750982137 <yomalthushara2295266@gmail.com>
2025-05-29 20:41:39 +0530

    Update server port to 5000 in server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.get("/api/users", (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
